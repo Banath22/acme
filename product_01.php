e45f27bd7f2600bf88e3427973f87c8ee252a1aa
siemdicou <siemdicou@siems-MacBook-Pro.local>
2015-03-30 13:25:55 +0200

    hai

--- product_01.php
+++ product_01.php
@@ -2,13 +2,17 @@
 <html>
 <head>
 	<title>product</title>
-	<style type="text/css" href="style.css"></style>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="script.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
 </head>
 <body>
+	<div class="wrapper">
+	<h1>hoi</h1>
 		<img src="img/tv1/p1.jpg">
 		<img src="img/tv1/p2.jpg">
 		<img src="img/tv1/p3.jpg">
 		<img src="img/tv1/p4.jpg">
-
+	</div>
 </body>
 </html>
