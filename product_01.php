ca69dcbb6ff4e319a0bb9e8feb5de627b197b5aa
siemdicou <siemdicou@siems-MacBook-Pro.local>
2015-03-30 12:05:22 +0200

    hai

---
+++ product_01.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>product</title>
+	<style type="text/css" href="style.css"></style>
+</head>
+<body>
+		<img src="img/tv1/p1.jpg">
+		<img src="img/tv1/p2.jpg">
+		<img src="img/tv1/p3.jpg">
+		<img src="img/tv1/p4.jpg">
+
+</body>
+</html>
