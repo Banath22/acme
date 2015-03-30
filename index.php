906e6f358066b1aab737198a38774cd34ed15cc1
Banath22 <casper_vz@hotmail.com>
2015-03-30 11:47:40 +0200

    hai

--- index.php
+++ index.php
@@ -9,76 +9,76 @@
 	<div class="wrapper">
 				<!-- tv 1 -->
 		<img src="img/tv1/p1.jpg">
-		<img src="img/tv1/p2.jpg">
+		<!-- <img src="img/tv1/p2.jpg">
 		<img src="img/tv1/p3.jpg">
-		<img src="img/tv1/p4.jpg">
+		<img src="img/tv1/p4.jpg"> -->
 
-				<br><br>
+				
 
 				<!-- tv2 -->
 		<img src="img/tv2/p1.png">
-		<img src="img/tv2/p2.png">
+		<!-- <img src="img/tv2/p2.png">
 		<img src="img/tv2/p3.png">
-		<img src="img/tv2/p4.png">
+		<img src="img/tv2/p4.png"> -->
 
-				<br><br>
+				
 
 				<!-- tv3 -->
 		<img src="img/tv3/p1.png">
-		<img src="img/tv3/p2.png">
-		<img src="img/tv3/p3.png">
+		<!-- <img src="img/tv3/p2.png">
+		<img src="img/tv3/p3.png"> -->
 
-				<br><br>
+				
 				<!-- tv4 -->
 		<img src="img/tv4/p1.png">
-		<img src="img/tv4/p2.png">
-		<img src="img/tv4/p3.png">
+		<!-- <img src="img/tv4/p2.png">
+		<img src="img/tv4/p3.png"> -->
 
-				<br><br>
+				
 				<!-- tv5 -->
 		<img src="img/tv5/p1.png">
-		<img src="img/tv5/p2.png">
-				<br><br>
+<!-- 		<img src="img/tv5/p2.png">
+ -->				
 				<!-- tv6 -->
 		<img src="img/tv6/p1.png">
-		<img src="img/tv6/p2.png">
+	<!-- 	<img src="img/tv6/p2.png">
 		<img src="img/tv6/p3.png">
-		<img src="img/tv6/p4.png">
-				<br><br>
+		<img src="img/tv6/p4.png"> -->
+				
 
 				<!-- tv7 -->
-		<!--<img src="img/tv7/01.png">
-		<img src="img/tv7/02.png">
+		<img src="img/tv7/01.png">
+	<!-- 	<img src="img/tv7/02.png">
 		<img src="img/tv7/03.png">
 		<img src="img/tv7/04.png">
-		<img src="img/tv7/05.png">
-				<br><br>
-				<!-- tv8 -->
-		<!--<img src="img/tv8/01.png">
-		<img src="img/tv8/02.png">
-		<img src="img/tv8/03.png">
+		<img src="img/tv7/05.png"> -->
+				
+				<!-- tv8  -->
+		<img src="img/tv8/01.png">
+	<!-- 	<img src="img/tv8/02.png">
+		<img src="img/tv8/03.png"> -->
 
-				<br><br>
+				
 				<!-- tv9 -->
-		<!--<img src="img/tv9/01.png">
-		<img src="img/tv9/02.png">
-		<img src="img/tv9/03.png">
+		<img src="img/tv9/01.png">
+	<!-- 	<img src="img/tv9/02.png">
+		<img src="img/tv9/03.png"> -->
 
-				<br><br>
-				<!-- tv10 -->
-		<!--<img src="img/tv10/01.png">
-		<img src="img/tv10/02.png">
-
-				<br><br>
+				
+				<!-- tv10-->
+		<img src="img/tv10/01.png">
+<!-- 		<img src="img/tv10/02.png">
+ -->
+				
 				<!-- tv11 -->
-		<!--<img src="img/tv11/01.png">
-		<img src="img/tv11/02.png">
-
-				<br><br>
-				<!-- tv12 -->
-		<!--<img src="img/tv12/01.png">
-		<img src="img/tv12/02.png">
-		<img src="img/tv12/03.png">-->
+		<img src="img/tv11/01.png">
+<!-- 		<img src="img/tv11/02.png">
+ -->
+				
+				<!-- tv12--> 
+		<img src="img/tv12/01.png">
+	<!-- 	<img src="img/tv12/02.png">
+		<img src="img/tv12/03.png"> -->
 
 
 	
