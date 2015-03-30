ff279f7c275f89ac87d97fdbd99206291077c5a0
Banath22 <casper_vz@hotmail.com>
2015-03-30 12:00:33 +0200

    hai

--- index.php
+++ index.php
@@ -4,86 +4,49 @@
 	<title>Sicas webshop!</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script type="text/javascript" src="script.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
 </head>
 <body>
+<div class="head">
+	<header><h1>Webshop SiCas</h1></header>
+</div>
 	<div class="wrapper">
 				<!-- tv 1 -->
 		<img src="img/tv1/p1.jpg">
-		<!-- <img src="img/tv1/p2.jpg">
-		<img src="img/tv1/p3.jpg">
-		<img src="img/tv1/p4.jpg"> -->
-
-				
 
 				<!-- tv2 -->
 		<img src="img/tv2/p1.png">
-		<!-- <img src="img/tv2/p2.png">
-		<img src="img/tv2/p3.png">
-		<img src="img/tv2/p4.png"> -->
-
-				
 
 				<!-- tv3 -->
 		<img src="img/tv3/p1.png">
-		<!-- <img src="img/tv3/p2.png">
-		<img src="img/tv3/p3.png"> -->
-
 				
 				<!-- tv4 -->
 		<img src="img/tv4/p1.png">
-		<!-- <img src="img/tv4/p2.png">
-		<img src="img/tv4/p3.png"> -->
-
 				
 				<!-- tv5 -->
 		<img src="img/tv5/p1.png">
-<!-- 		<img src="img/tv5/p2.png">
- -->				
+	
 				<!-- tv6 -->
 		<img src="img/tv6/p1.png">
-	<!-- 	<img src="img/tv6/p2.png">
-		<img src="img/tv6/p3.png">
-		<img src="img/tv6/p4.png"> -->
-				
 
 				<!-- tv7 -->
 		<img src="img/tv7/01.png">
-	<!-- 	<img src="img/tv7/02.png">
-		<img src="img/tv7/03.png">
-		<img src="img/tv7/04.png">
-		<img src="img/tv7/05.png"> -->
-				
+
 				<!-- tv8  -->
 		<img src="img/tv8/01.png">
-	<!-- 	<img src="img/tv8/02.png">
-		<img src="img/tv8/03.png"> -->
 
-				
 				<!-- tv9 -->
 		<img src="img/tv9/01.png">
-	<!-- 	<img src="img/tv9/02.png">
-		<img src="img/tv9/03.png"> -->
 
-				
 				<!-- tv10-->
 		<img src="img/tv10/01.png">
-<!-- 		<img src="img/tv10/02.png">
- -->
-				
+
 				<!-- tv11 -->
 		<img src="img/tv11/01.png">
-<!-- 		<img src="img/tv11/02.png">
- -->
-				
+
 				<!-- tv12--> 
 		<img src="img/tv12/01.png">
-	<!-- 	<img src="img/tv12/02.png">
-		<img src="img/tv12/03.png"> -->
 
-
-	
-
-		
 
 	</div>
 </body>
