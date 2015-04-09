6a64f50b6f8e1903d15dd41cf34aa453c17afdbd
Banath22 <casper_vz@hotmail.com>
2015-04-09 16:36:26 +0200

    hai

--- acmeconnect.php
+++ acmeconnect.php
@@ -1,11 +1,3 @@
 <?php
-
-$mysqli = new mysqli('localhost','root','appeltje','acme');
-
-// if($mysqli -> connect_errno){
-// 	echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ')' . $mysqli->connect_error;
-// }
-// else{
-// 	echo'connection ok';
-// }
+$mysqli = new mysqli('127.0.0.1','admin_18070_acme','appeltje','18070_acme');
 ?>
