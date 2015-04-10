9beb574f3e6b08774228ce53f0f501f98130840d
Banath22 <casper_vz@hotmail.com>
2015-04-10 12:19:34 +0200

    hi

--- acmeconnect.php
+++ acmeconnect.php
@@ -1,8 +1,8 @@
 <?php
 
-// $mysqli = new mysqli('localhost','root','appeltje','acme');
+$mysqli = new mysqli('localhost','root','appeltje','acme');
 
-$mysqli = new mysqli('127.0.0.1','admin_18070_acme','appeltje','18070_acme');
+// $mysqli = new mysqli('127.0.0.1','admin_18070_acme','appeltje','18070_acme');
 
 // if($mysqli -> connect_errno){
 // 	echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ')' . $mysqli->connect_error;
